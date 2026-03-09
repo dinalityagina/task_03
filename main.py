3c9a73f9bd9a15652738b36104e3de603176c199
dinalityagina <dina.lityagina@gmail.com>
2026-03-09 21:58:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pandas as pd
 from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel, Field
 from typing import List
-from main import load_data, save_data
 
 app = FastAPI(title="Task_03 API")
 
@@ -76,6 +75,7 @@ async def delete_record(record_id: int):
         status_code=status.HTTP_404_NOT_FOUND,
         detail=f"Запись с id {record_id} не найдена"
     )
+
 
 
 
